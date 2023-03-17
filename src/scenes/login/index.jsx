870958af6c5509cfc1705a56fd56a22dc81740e1
JesusMendezO <46574599+JesusMendezO@users.noreply.github.com>
2023-03-17 14:23:34 -0400

    Update index.jsx

--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -141,7 +141,7 @@ export default function Login() {
             >
             </Box>
             <Typography className='mt-4' component="h1" variant="h5">
-              Ingresa Tu cuenta
+              Ingresa Tu Cuenta
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
