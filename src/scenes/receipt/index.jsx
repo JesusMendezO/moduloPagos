9dad3b894768949e0e262c10b027000a6f18bcb3
JesusMendezO <46574599+JesusMendezO@users.noreply.github.com>
2023-03-17 14:25:20 -0400

    Update index.jsx

--- src/scenes/receipt/index.jsx
+++ src/scenes/receipt/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
