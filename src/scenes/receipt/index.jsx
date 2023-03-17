82865a97f541324a874d394e7065ea8d4e8340f4
JesusMendezO <46574599+JesusMendezO@users.noreply.github.com>
2023-03-17 14:44:32 -0400

    Update index.jsx

--- src/scenes/receipt/index.jsx
+++ src/scenes/receipt/index.jsx
@@ -86,7 +86,7 @@ const [datos, setDatos] = useState('')
       </AppBar>
       </Container>
       <Box>
-        <Search details={datos.data}/>
+        <Search details={initialDetails}/>
       </Box>
       <Footer />
     </Box> 
