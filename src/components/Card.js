95cf0cdc2094ba459a9a60f9e5fc8022ee176fd0
JesusMendezO <46574599+JesusMendezO@users.noreply.github.com>
2023-03-21 14:09:15 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -82,10 +82,10 @@ const [inf,setInf]=useState([]);
     }
 let vacio =[];
 let  l ;
-  let sum;
+  let sum =0;
     const enviar = e => {
       for (let i = 0; i < lang.length ; i++) {
-        sum = person.incntivo+sum
+        sum = Number(person.incntivo)+sum
         console.log(lang)
          l = [{ value: lang[i], nombre: person.nombre , numeroRecibo: person.cedula , monto: person.incntivo , total: sum}];
        
