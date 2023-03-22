3885bcff2824cda677122f280d7579b21e9887db
JesusMendezO <46574599+JesusMendezO@users.noreply.github.com>
2023-03-22 13:41:20 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -99,7 +99,7 @@ let  l ;
 const d = new Date(new Date(new Date().setMonth(mes)));
 let monthName = month[d.getMonth()];
 
-         l = [{ value: lang[i], nombre: person.nombre , numeroRecibo: person.cedula , monto: person.incntivo,fechaEj:date , total: sum, mes: monthName }];
+         l = [{ value: lang[i], nombre: person.nombre , numeroRecibo: person.recibo , monto: person.incentivo,fechaEj:date , total: sum, mes: monthName, banco:person.banco, proyecto:person.proyecto, cargo:person.cargo }];
        
       vacio = vacio.concat(l)
     }
