2f1da2c4c98895012741681729ca6059704fd0ee
japirela <126787080+japirela@users.noreply.github.com>
2023-03-22 08:35:35 -0400

    Update index.jsx

--- src/scenes/receipt/index.jsx
+++ src/scenes/receipt/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { useState } from 'react'
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import Search from 'components/Search';
 import Footer from 'components/Footer';
 import initialDetails from 'data/initialDetails';
 import Container from '@mui/material/Container';
-import LogoutOutlined from '@mui/icons-material/LogoutOutlined';
+import NavBar from 'components/NavBar';
 import clienteAxios from '../../config/clienteAxios'
+
 function Receipt() {
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -38,53 +35,12 @@ const [datos, setDatos] = useState([])
     // always executed
   });
 
-  
   console.log(datos);
   console.log(initialDetails)
-  
-  
+   
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Container>
-      <AppBar position="fixed" sx={{ bgcolor:"teal" }}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Recibos de Pago
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <LogoutOutlined />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}> Cerrar Sesión </MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-      </Container>
+      <Navbar>
       <Box>
         <Search details={datos}/>
       </Box>
