8f530848664c10bfe85661ed30ac0b0cc21432d5
japirela <126787080+japirela@users.noreply.github.com>
2023-10-31 18:49:19 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -97,7 +97,7 @@ let  l ;
       for (let i = 0; i < lang.length ; i++) {
         sum = Number(person.incentivo)+sum
 	      
-         mes = Number(lang[i].slice(3, 4));
+         mes = Number(lang[i].slice(3, 4)-2);
 	 const month = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
          const d = new Date(new Date(new Date().setMonth(mes)));
          monthName =  monthName +", "+ month[d.getMonth()]  ;
