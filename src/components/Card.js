d34ecc425300abf5dce5f3fc1eed3088e7508742
JesusMendezO <46574599+JesusMendezO@users.noreply.github.com>
2023-10-31 23:48:08 -0300

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -96,6 +96,7 @@ let mes;
         sum = Number(person.incentivo)+sum
 	      
          mes = Number(lang[i].slice(3, 4)-1);
+	      console.log(mes)
 	 const month = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
          const d = new Date(new Date(new Date().setMonth(mes)));
 	 //const d = new Date(new Date().setMonth(mes));
