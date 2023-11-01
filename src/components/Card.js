072d6e56d82600f9f34f03529d63710c4b90616a
japirela <126787080+japirela@users.noreply.github.com>
2023-10-31 20:09:29 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -99,7 +99,7 @@ let mes;
 	 const month = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
          //const d = new Date(new Date(new Date().setMonth(mes)));
 	 const d = new Date(new Date().setMonth(mes));
-         monthName =  monthName +", "+ month[d.getMonth()-1]  ;
+         monthName =  monthName +", "+ month[d.getMonth(0)]  ;
 
          l = [{ value: lang[i], nombre: person.nombre ,titular:person.titular, numeroRecibo: person.recibo , monto: person.incentivo,fechaEj:date , total: sum, mes: monthName, banco:person.banco, proyecto:person.proyecto, cargo:person.cargo,cedula:person.cedula }];
        
