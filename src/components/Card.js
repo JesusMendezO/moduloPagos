a78dfa4d6b8cd678a46f63256eb79f267cadf13d
japirela <126787080+japirela@users.noreply.github.com>
2023-11-01 06:13:37 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -38,7 +38,7 @@ const style = {
 // *************************************** PERIODOS DE PAGO *********************************************
 
 var year = new Date().getFullYear().toString()
-var enero = new Date(new Date(new Date().setMonth(1)).setDate(0)).getDate()+ '/01/' + year ;
+var enero = new Date(new Date(new Date().setMonth(1)).setDate(-1)).getDate()+ '/01/' + year ;
 var febrero = new Date(new Date(new Date().setMonth(2)).setDate(0)).getDate()+ '/02/' + year ;
 var marzo = new Date(new Date(new Date().setMonth(3)).setDate(-1)).getDate()+ '/03/' + year ;
 var abril = new Date(new Date(new Date().setMonth(4)).setDate(0)).getDate()+ '/04/' + year ;
